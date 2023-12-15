4e8660fcbd2d11649f00abe840db0c88de507d94
Masud Afsar <masud.afsar@gmail.com>
2023-12-15 20:06:31 +0330

    Implement Hello input name in session 02 module

---
+++ Session02/src/info/afsar/java/HelloInputName.java
@@ -0,0 +1,13 @@
+package info.afsar.java;
+
+import java.util.Scanner;
+
+public class HelloInputName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name:");
+        String userInput = scanner.nextLine();
+        System.out.println("Hello dear " + userInput);
+    }
+}

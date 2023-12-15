8c59e69503b6cdb65499f80548ad62023f29b015
Masud Afsar <masud.afsar@gmail.com>
2023-12-15 19:48:19 +0330

    Implement sesstion 01 tasks

---
+++ Session01/src/info/afsar/java/BasicHelloWorld.java
@@ -0,0 +1,7 @@
+package info.afsar.java;
+
+public class BasicHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Session01/src/info/afsar/java/HelloNamesInArg.java
@@ -0,0 +1,13 @@
+package info.afsar.java;
+
+public class HelloNamesInArg {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hello Names application.");
+
+        for (String name : args) {
+            System.out.println(" - Hello dear " + name);
+        }
+
+        System.out.println("Have a nice day!");
+    }
+}
